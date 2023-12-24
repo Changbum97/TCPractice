695806a9ff9a3f8a67db8bcb6729c2217d665dbb
Changbum97 <chb2005@naver.com>
2023-12-24 21:00:02 +0900

    test: Item Controller Test 구현

---
+++ src/test/java/com/study/tcpractice/controller/ItemControllerTest.java
@@ -0,0 +1,127 @@
+package com.study.tcpractice.controller;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.tcpractice.service.ItemService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static com.study.tcpractice.testModels.ItemTestModel.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ItemController.class)
+class ItemControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ItemService itemService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    /**
+     * Price가 Json에서 출력될 때, 가격 포맷을 적용하도록 해줬는데
+     * Test 과정에서 Request도 String으로 바뀌는 문제 발생
+     *   => Test Code에서는 가격 포맷 적용 X
+     */
+    @BeforeEach
+    void nonApplyPriceUtil() {
+        objectMapper.configure(MapperFeature.USE_ANNOTATIONS, false);
+    }
+
+
+    @Test
+    @DisplayName("Add Item Success Test")
+    void addItemSuccess() throws Exception {
+
+        // when
+        when(itemService.saveItem(any())).thenReturn(itemResponse1);
+
+        // then
+        mockMvc.perform(post("/items")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(itemRequest1))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value(itemName1))
+                .andExpect(jsonPath("$.price").value(itemPrice1));
+    }
+
+    @Test
+    @DisplayName("Edit Item Success Test")
+    void editItemSuccess() throws Exception {
+
+        // when
+        when(itemService.editItem(any())).thenReturn(itemResponse1);
+
+        // then
+        mockMvc.perform(put("/items")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(itemRequest1))
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.name").value(itemName1))
+                .andExpect(jsonPath("$.price").value(itemPrice1));
+    }
+
+    @Test
+    @DisplayName("Delete Item Success Test")
+    void deleteItemSuccess() throws Exception {
+
+        // when
+        when(itemService.deleteItem(any())).thenReturn("1번 Item이 삭제되었습니다.");
+
+        // then
+        mockMvc.perform(delete("/items/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("1번 Item이 삭제되었습니다."));
+    }
+
+    @Test
+    @DisplayName("Delete Item Fail Test")
+    void deleteItemFail() throws Exception {
+
+        // when
+        when(itemService.deleteItem(any())).thenReturn("id에 해당하는 Item이 없습니다.");
+
+        // then
+        mockMvc.perform(delete("/items/2"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("id에 해당하는 Item이 없습니다."));
+    }
+
+    @Test
+    @DisplayName("Find All Item Success Test")
+    void findAllItemSuccess() throws Exception {
+        // when
+        when(itemService.findAll()).thenReturn(itemDtos);
+
+        // then
+        mockMvc.perform(get("/items"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(2))
+                .andExpect(jsonPath("$[0].name").value(itemName1))
+                .andExpect(jsonPath("$[1].name").value(itemName2));
+    }
+}
--- src/test/java/com/study/tcpractice/testModels/ItemTestModel.java
+++ src/test/java/com/study/tcpractice/testModels/ItemTestModel.java
@@ -2,6 +2,8 @@ package com.study.tcpractice.testModels;
 
 import com.study.tcpractice.domain.dto.ItemDto;
 
+import java.util.List;
+
 public class ItemTestModel {
 
     public static final String itemName1 = "가방";
@@ -10,8 +12,9 @@ public class ItemTestModel {
     public static final String itemName2 = "신발";
     public static final Integer itemPrice2 = 72000;
 
-    public static final ItemDto item1 = ItemDto.builder().id(1L).name(itemName1).price(itemPrice1).build();
-    public static final ItemDto item2 = ItemDto.builder().id(2L).name(itemName2).price(itemPrice2).build();
+    public static final ItemDto itemRequest1 = ItemDto.builder().name(itemName1).price(itemPrice1).build();
+    public static final ItemDto itemResponse1 = ItemDto.builder().id(1L).name(itemName1).price(itemPrice1).build();
+    public static final ItemDto itemResponse2 = ItemDto.builder().id(2L).name(itemName2).price(itemPrice2).build();
 
-
+    public static final List<ItemDto> itemDtos = List.of(itemResponse1, itemResponse2);
 }
