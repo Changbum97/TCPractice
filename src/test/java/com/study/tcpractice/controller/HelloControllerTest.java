cd68b953d056609ddcbbc4a2ee3c3eec50c6b652
Changbum97 <chb2005@naver.com>
2023-12-24 19:11:50 +0900

    test: Hello Controller Test Code 추가

---
+++ src/test/java/com/study/tcpractice/controller/HelloControllerTest.java
@@ -0,0 +1,27 @@
+package com.study.tcpractice.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Health Check Test Success")
+    void healthCheckSuccess() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello"));
+    }
+
+}
